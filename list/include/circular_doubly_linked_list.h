2d302e4a9a2b05dde97159bd189916ac626b23ad
Ramon Almeida <ramongoncalves76@gmail.com>
2021-12-24 15:13:47 -0300

    Complete division of functions and files in DSList

--- list/include/circular_doubly_linked_list.h
+++ list/include/circular_doubly_linked_list.h
@@ -1,16 +1,50 @@
 /**
  * @author Ytalo Ramon
- * @date   16/11/2021
+ * @date   24/12/2021
  */
 
 #include "doubly_linked_list_base.h"
 
 /**
- * struct _List.
- * attr: length - Number of elements currently present in the lists.
- * attr: head - Head List.
+ * Remove the element after a base item.
+ * @param item_base Reference item.
+ * @param new_item Item for insertion.
  */
-struct _List {
-    int length;
-    struct _ItemList head;
-};
+void cdll_insert_after_item(ItemDLList *item_base, ItemDLList *new_item);
+
+/**
+ * Remove the element after a base item.
+ * @param item_base Reference item.
+ */
+void cdll_remove_after_item(ItemDLList *item_base);
+
+/**
+ * Returns the number of items with the specified value.
+ * @param item_start Pointer to start element.
+ * @param elmnt Element sought.
+ * @param comparator Comparison function.
+ *      Input in comparator(elmnt, itemList_current).
+ * @return Number of elements that satisfy the conditions.
+ */
+size_t cdll_count(ItemDLList *item_start, void *elmnt, ICOMPARATOR);
+
+/**
+ * Returns the first ItemList with the specified value.
+ * @param item_start Pointer to start element.
+ * @param elmnt Element sought.
+ * @param comparator Comparison function.
+ *      Input in comparator(elmnt, itemList_current).
+ * @return The object if found, or NULL if not.
+ */
+ItemDLList *cdll_find(ItemDLList *item_start, void *elmnt, ICOMPARATOR);
+
+/**
+ * Returns the offset bettween the item_start
+ *  and the element with the specified value.
+ * @param item_start Pointer to start element.
+ * @param elmnt Element sought.
+ * @param comparator Comparison function.
+ *      Input in comparator(elmnt, itemList_current).
+ * @return Offset object if found, or -1 if not.
+ */
+int cdll_offset(ItemDLList *item_start, void *elmnt, ICOMPARATOR);
